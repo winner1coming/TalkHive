d83f3b026e2bd0c547d9e60e39cf3b6cf342868d
覃锦玲 <2698553217@qq.com>
2024-11-18 05:37:15 +0000

    rename Frontend/src/views/.keep to Frontend/src/views/Home.vue. 主页面视图
    Signed-off-by: 覃锦玲 <2698553217@qq.com>

---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>欢迎来到聊天室</h1>
+    <router-link to="/messages">消息列表</router-link>
+    <router-link to="/friends">好友列表</router-link>
+    <router-link to="/groups">群聊列表</router-link>
+    <router-link to="/notes">笔记</router-link>
+    <router-link to="/favorites">收藏</router-link>
+    <router-link to="/ddls">DDL</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
