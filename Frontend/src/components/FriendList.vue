0b18b9c11331d2f87f8c952f919255365a041a3a
覃锦玲 <2698553217@qq.com>
2024-11-18 05:40:54 +0000

    add Frontend/src/components/FriendList.vue. 好友列表组件
    Signed-off-by: 覃锦玲 <2698553217@qq.com>

---
+++ Frontend/src/components/FriendList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="friend-list">
+    <h2>好友列表</h2>
+    <ul>
+      <li v-for="friend in friends" :key="friend.id">
+        {{ friend.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getFriends } from '../services/api';
+
+export default {
+  name: 'FriendList',
+  data() {
+    return {
+      friends: [],
+    };
+  },
+  methods: {
+    async fetchFriends() {
+      const response = await getFriends();
+      this.friends = response.data;
+    },
+  },
+  created() {
+    this.fetchFriends();
+  },
+};
+</script>
+
+<style scoped>
+.friend-list {
+  padding: 20px;
+}
+</style>
