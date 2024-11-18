c8276dc2d2e9f4eaa1d2d443a82c003859464f84
覃锦玲 <2698553217@qq.com>
2024-11-18 06:05:58 +0000

    通讯录功能区域
    Signed-off-by: 覃锦玲 <2698553217@qq.com>

---
+++ Frontend/src/components/Contact_list/GroupRequest.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="group-request">
+    <h2>群聊申请</h2>
+    <ul>
+      <li v-for="request in requests" :key="request.id">
+        {{ request.name }}
+        <button @click="acceptRequest(request.id)">接受</button>
+        <button @click="rejectRequest(request.id)">拒绝</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getGroupRequests, acceptGroupRequest, rejectGroupRequest } from '../services/api';
+
+export default {
+  name: 'GroupRequest',
+  data() {
+    return {
+      requests: [],
+    };
+  },
+  methods: {
+    async fetchRequests() {
+      const response = await getGroupRequests();
+      this.requests = response.data;
+    },
+    async acceptRequest(requestId) {
+      await acceptGroupRequest(requestId);
+      this.fetchRequests();
+    },
+    async rejectRequest(requestId) {
+      await rejectGroupRequest(requestId);
+      this.fetchRequests();
+    },
+  },
+  created() {
+    this.fetchRequests();
+  },
+};
+</script>
+
+<style scoped>
+.group-request {
+  padding: 20px;
+}
+</style>
